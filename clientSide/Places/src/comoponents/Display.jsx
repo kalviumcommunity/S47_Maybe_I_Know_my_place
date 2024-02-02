66d5a18371b8ba72cf3c37a4b2d2197f1d789a84
Surya <thesuryapratap2003@gmail.com>
2024-02-02 16:09:20 +0530

    Connected

--- clientSide/Places/src/comoponents/Display.jsx
+++ clientSide/Places/src/comoponents/Display.jsx
@@ -1,33 +1,50 @@
-import React from 'react'
-import data from '../places.json'
-import '../comoponents/components.css'
-import tm from '../assets/tajMahal.jpg'
-const Display = (props) => {
+import React from 'react';
+import '../comoponents/components.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+
+
+const Display = () => {
+
+    const [Myuser, setUser] = useState([])
+    useEffect(() => {
+        axios.get('http://localhost:3000/display')
+            .then(users => {
+                setUser(users.data);
+                console.log(users)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <div>
-            {/* {
-                data.map((item) => {
-                    return(
-                        <div key={item.id}>
-                            <h1>Place Name: {item.Topic} </h1>
-                            <p className='state'> <b> State: </b> {item.State}</p>
-                            <p> <b> City: </b> {item.City}</p>
-                            <b> <p>Build on: {item.Year_of_build}</p> </b>
-                            <p> <b> Description: </b> {item.Description} </p>
-                            <img src={item.image} alt="" />
-                            <hr />
+            {Myuser.map((item) => (
+                <div key={item.id}>
+                    <div className='container'>
+                        <div>
+                            <h1>Place Name: {item.Topic}</h1>
+                            <p className='state'>
+                                <b>State:</b> {item.State}
+                            </p>
+                            <p>
+                                <b>City:</b> {item.City}
+                            </p>
+                            <b>
+                                <p>Build on: {item.Year_of_build}</p>
+                            </b>
+                            <p>
+                                <b>Description:</b> {item.Description}
+                            </p>
                         </div>
-                    )
-                })
-            } */}
-            <h1>Place Name: {props.Topic} </h1>
-            <p className='state'> <b> State: </b> {props.State}</p>
-            <p> <b> City: </b> {props.City}</p>
-            <b> <p>Build on: {props.Year_of_build}</p> </b>
-            <p> <b> Description: </b> {props.Description} </p>
-            <img src={tm} alt="" />
+                        <div>
+                            <img src={item.img} alt='' className='aii' />
+                        </div>
+                    </div>
+                    <hr />
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
 
-export default Display
+export default Display;
